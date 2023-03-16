2675098b5213ba87f43056eb8b772fcbda80ba4e
Jtenux <amiherejocelyn@gmail.com>
2023-03-16 15:22:19 -0700

    Intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - Entry Point
  * 
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
