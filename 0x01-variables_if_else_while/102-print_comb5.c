5539ae7121d66524c81da7664959d93e60933c4d
Jtenux <amiherejocelyn@gmail.com>
2023-03-17 16:37:30 -0700

    Two different numbers with for different combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - this prints all possible combos of to different numbers
+ *
+ * Return: always 0 whem successful
+ */
+
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 0 ; m < 100 ; m++)
+	{
+		for (n = 0 ; n < 100 ; n++)
+		{
+			if (m < n)
+			{
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				putchar(' ');
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				if (m != 98 || n != 99)
+				{
+					putchar('.');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
