f6abea4d44880cf9a8aff405568796c60c403a99
Jtenux <amiherejocelyn@gmail.com>
2023-03-16 17:41:16 -0700

    quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - this prints what the user wants to see
+ *
+ * Return: Always 1 when successful
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
