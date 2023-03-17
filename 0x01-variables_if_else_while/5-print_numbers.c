19929506f8cd96f1c5d3694d4656e64f227d4f2f
Jtenux <amiherejocelyn@gmail.com>
2023-03-17 13:10:58 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digits of base10
+ *
+ * Return: always 0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
