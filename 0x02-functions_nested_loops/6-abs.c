3d71dc73063ec5817d1e99ac2ce346bf4f69af47
Jtenux <amiherejocelyn@gmail.com>
2023-03-21 16:34:45 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - this computes the absolut value of an integer
+ * @i: integer to check
+ * Return: absolute value of integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+		return (-i);
+}
+
