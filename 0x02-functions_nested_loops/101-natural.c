fb9d34e3f5510e783c28091a61a80a11dfd70c58
Jtenux <amiherejocelyn@gmail.com>
2023-03-21 20:12:11 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints natural numbers
+ *
+ * Return: always 0 when succesful
+ */
+int main(void)
+{
+	int m;
+	int n = 0;
+
+	while (m < 1024)
+	{
+		if ((m % == 0) || (m % 5 == 0))
+		{
+			n += m;
+		}
+		m++
+	}
+	printf("%d\n", n);
+	return (0);
+}
