fe5fa941b089a5c1eb2faa45ca3ef7e7e0bc2985
Jtenux <amiherejocelyn@gmail.com>
2023-03-23 10:18:06 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - this function multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: always 0 when successful
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
