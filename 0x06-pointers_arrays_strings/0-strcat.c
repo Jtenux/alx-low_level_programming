d0145b2c33d3208786fa6636c8d2c2fda64fbd48
Jtenux <amiherejocelyn@gmail.com>
2023-03-30 12:48:15 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,20 @@ char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (c = 0; src[c] != '\0'; c++)
+	c = 0;
+	for (i = 0; i < 10000; i++)
 	{
-		dest[i] = src[c];
-		i++;
+		if (dest[i] == '\0')
+			break;
+		c++;
 	}
 
-	dest[i] = '\0';
+	for (i = 0; src[i] != '\0' && i < c; i++)
+	{
+		dest[c + i] = src[i];
+	}
+
+	dest[c + i] = '\0';
+
 	return (dest);
 }
