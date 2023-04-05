75b6672b5ca0f0d40091fa1a0f15bcb5b806479d
Jtenux <amiherejocelyn@gmail.com>
2023-04-05 11:50:18 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * pow_operator  - this function returns the suare root of a number
+ * @n: number
+ * @s: number
+ * Return: -1 when the function does not have a root
+ *
+ */
+
+int pow_operator(int n, int s)
+{
+	if (s % (n / s) == 0)
+	{
+		return (0 + pow_operator(n, s + 1));
+	}
+	else if (s * (n / s) == n)
+	{
+		return (s);
+	}
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - this functon returns the natural root of a number
+ * @n: given number
+ * Return: natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	return (pow_operator(n, 2));
+}
+
+
