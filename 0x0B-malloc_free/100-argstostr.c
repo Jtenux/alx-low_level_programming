517ea942e246eec93952f4fac5d2b1d16515078e
Jtenux <amiherejocelyn@gmail.com>
2023-04-11 14:03:02 -0700

    malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - main function
+ * @ac: aug 1
+ * @av: aug 2
+ * Return: pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int len = 0;
+	int m;
+	int pos = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (m = 0 ; m < ac ; m++)
+	{
+		len += strlen(av[m]) + 1;
+	}
+	aout = malloc(sizeof(char) * len);
+	if (aout == NULL)
+		return (NULL);
+	for (m = 0 ; m < ac ; m++)
+	{
+		strcpy(aout + pos, av[m]);
+		pos += strlen(av[m]);
+		aout[pos++] = '\n';
+	}
+	aout[pos] = '\0';
+	return (aout);
+}
+
