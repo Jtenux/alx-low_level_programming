a14ec840c17932facdadad06ecca2738e1020134
Jtenux <amiherejocelyn@gmail.com>
2023-04-17 13:00:51 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog2;
+
+	dog2 = malloc(sizeof(struct dog));
+	if (dog2 == NULL)
+		return (NULL);
+
+	dog2->name = malloc(strlen(name) + 1);
+	if (dog2->name == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dog2->name, name);
+	dog2->age = age;
+	dog2->owner = malloc(strlen(owner) + 1);
+	if (dog2->owner == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dog2->owner, owner);
+	return (dog2);
+}
